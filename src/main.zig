d7dc5f9935262638300392b79bfb73f77f075895
Ryoga-exe <git@ryoga.dev>
2026-09-08 17:09:03 +0900

    feat: execute shell input through runtime

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,18 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const habush = @import("habush");
 const Io = std.Io;
 const platform = @import("platform.zig");
 
 pub fn main(init: std.process.Init) !void {
+    const status = try runApplication(init);
+    if (status != 0) std.process.exit(status);
+}
+
+fn runApplication(init: std.process.Init) !u8 {
     const arena = init.arena.allocator();
     const gpa = init.gpa;
     const io = init.io;
-
-    const args = try init.minimal.args.toSlice(arena);
-    for (args) |arg| {
-        std.log.info("arg: {s}", .{arg});
-    }
 
     const interactive = try Io.File.stdin().isTty(io) and try Io.File.stderr().isTty(io);
 
@@ -21,29 +22,73 @@ pub fn main(init: std.process.Init) !void {
 
     var stdin_buffer: [4096]u8 = undefined;
     var stdin_file_reader: Io.File.Reader = .initStreaming(.stdin(), io, &stdin_buffer);
+    var stdout_buffer: [0]u8 = .{};
+    var stdout_file_writer = Io.File.stdout().writerStreaming(io, &stdout_buffer);
+    var stderr_buffer: [0]u8 = .{};
+    var stderr_file_writer = Io.File.stderr().writerStreaming(io, &stderr_buffer);
+
+    const cwd = try std.process.currentPathAlloc(io, arena);
+    const variables = try environmentBindings(arena, init.environ_map);
+    const search_path = if (init.environ_map.get("PATH")) |path|
+        try splitEnvironmentList(arena, path, std.fs.path.delimiter)
+    else
+        &.{};
+    const executable_extensions = if (builtin.os.tag == .windows)
+        if (init.environ_map.get("PATHEXT")) |extensions|
+            try splitEnvironmentList(arena, extensions, std.fs.path.delimiter)
+        else
+            default_executable_extensions
+    else
+        &.{};
+
+    var system_host: habush.Host.System = .{
+        .gpa = gpa,
+        .io = io,
+        .environ_map = init.environ_map,
+    };
+    defer system_host.deinit();
+    var system_resolver: habush.CommandResolver.System = .{
+        .io = io,
+        .executable_extensions = executable_extensions,
+    };
+    var session = try habush.Session.init(gpa, system_host.host(), .{
+        .resolver = system_resolver.resolver(),
+        .cwd = cwd,
+        .search_path = search_path,
+        .variables = variables,
+        .io = .{
+            .stdout = &stdout_file_writer.interface,
+            .stderr = &stderr_file_writer.interface,
+            .diagnostic_options = .{ .program_name = "habush" },
+        },
+    });
+    defer session.deinit();
 
     var shell: Shell = .{
-        .io = io,
         .allocator = gpa,
         .stdin = &stdin_file_reader.interface,
+        .stderr = &stderr_file_writer.interface,
+        .session = &session,
         .interactive = interactive,
     };
 
-    try shell.run();
+    return shell.run();
 }
 
 const Shell = struct {
-    io: Io,
     allocator: std.mem.Allocator,
     stdin: *Io.Reader,
+    stderr: *Io.Writer,
+    session: *habush.Session,
     interactive: bool,
+    last_status: u8 = 0,
 
     const LoopAction = enum {
         @"continue",
         exit,
     };
 
-    fn run(self: *Shell) !void {
+    fn run(self: *Shell) !u8 {
         while (true) {
             if (self.interactive) {
                 try self.printPrompt();
@@ -51,9 +96,9 @@ const Shell = struct {
 
             const source = try readLineAlloc(self.stdin, self.allocator) orelse {
                 if (self.interactive) {
-                    try Io.File.stderr().writeStreamingAll(self.io, "\n");
+                    try self.stderr.writeByte('\n');
                 }
-                break;
+                return self.last_status;
             };
             defer self.allocator.free(source);
 
@@ -63,19 +108,19 @@ const Shell = struct {
 
             switch (try self.handleInput(source)) {
                 .@"continue" => continue,
-                .exit => break,
+                .exit => return self.last_status,
             }
         }
     }
 
     fn printPrompt(self: *Shell) !void {
-        try Io.File.stderr().writeStreamingAll(self.io, "habush> ");
+        try self.stderr.writeAll("habush> ");
+        try self.stderr.flush();
     }
 
     fn handleInput(self: *Shell, source: [:0]const u8) !LoopAction {
         var arena = std.heap.ArenaAllocator.init(self.allocator);
         defer arena.deinit();
-
         const allocator = arena.allocator();
 
         if (std.mem.eql(u8, std.mem.trim(u8, source, &std.ascii.whitespace), "exit")) {
@@ -84,14 +129,81 @@ const Shell = struct {
 
         var tree = try habush.Ast.parse(allocator, source);
         defer tree.deinit(allocator);
+        if (tree.errors.len != 0) {
+            for (tree.errors) |parse_error| {
+                const location = tree.tokenLocation(0, parse_error.token);
+                try self.stderr.print("habush: {d}:{d}: ", .{
+                    location.line + 1,
+                    location.column + 1,
+                });
+                try tree.renderError(parse_error, self.stderr);
+                try self.stderr.writeByte('\n');
+            }
+            self.last_status = 2;
+            return .@"continue";
+        }
+        switch (tree.status) {
+            .complete => {},
+            .incomplete => {
+                try self.stderr.writeAll("habush: incomplete input\n");
+                self.last_status = 2;
+                return .@"continue";
+            },
+        }
 
-        var dump: Io.Writer.Allocating = .init(allocator);
-        defer dump.deinit();
-        try tree.dump(&dump.writer);
-        try Io.File.stdout().writeStreamingAll(self.io, dump.written());
+        var hir = try habush.AstGen.generate(allocator, tree);
+        defer hir.deinit(allocator);
+        const result = self.session.execute(hir) catch |err| switch (err) {
+            error.UnsupportedInstruction,
+            error.FieldSplittingUnsupported,
+            error.PathnameExpansionUnsupported,
+            error.TildeExpansionUnsupported,
+            error.ParameterExpansionUnsupported,
+            => {
+                try self.stderr.print("habush: unsupported runtime feature: {s}\n", .{
+                    @errorName(err),
+                });
+                self.last_status = 2;
+                return .@"continue";
+            },
+            else => |other| return other,
+        };
+        self.last_status = result.status;
 
         return .@"continue";
     }
+};
+
+fn environmentBindings(
+    allocator: std.mem.Allocator,
+    environ_map: *const std.process.Environ.Map,
+) ![]const habush.VariableStore.Binding {
+    const bindings = try allocator.alloc(habush.VariableStore.Binding, environ_map.count());
+    var len: usize = 0;
+    for (environ_map.keys(), environ_map.values()) |name, value| {
+        if (!habush.VariableStore.isValidName(name)) continue;
+        bindings[len] = .{ .name = name, .value = value, .exported = true };
+        len += 1;
+    }
+    return bindings[0..len];
+}
+
+fn splitEnvironmentList(
+    allocator: std.mem.Allocator,
+    value: []const u8,
+    delimiter: u8,
+) ![]const []const u8 {
+    var result: std.ArrayList([]const u8) = .empty;
+    var iterator = std.mem.splitScalar(u8, value, delimiter);
+    while (iterator.next()) |item| try result.append(allocator, item);
+    return result.toOwnedSlice(allocator);
+}
+
+const default_executable_extensions: []const []const u8 = &.{
+    ".COM",
+    ".EXE",
+    ".BAT",
+    ".CMD",
 };
 
 fn readLineAlloc(reader: *Io.Reader, allocator: std.mem.Allocator) !?[:0]u8 {
